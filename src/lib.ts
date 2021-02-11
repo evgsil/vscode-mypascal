e86eb6cdc91fa312f6681653cfd2b0db18ede474
Evgen Sylenko <evgen_sylenko@epam.com>
2021-02-11 19:18:13 +0200

    find dfm element

---
+++ src/lib.ts
@@ -0,0 +1,65 @@
+import { off } from "process";
+import * as vscode from "vscode";
+
+export const focusWord = (editor: vscode.TextEditor, word: string) => {
+  const offset = editor.document.getText().search(new RegExp(word));
+
+  if (offset < 0) {
+    return;
+  }
+
+  const newPos = editor.document.positionAt(offset);
+  editor.selection = new vscode.Selection(newPos, newPos);
+  editor.revealRange(
+    new vscode.Range(
+      newPos.line > 20 ? newPos.translate(-20, 0) : newPos,
+      newPos.translate(20, 0)
+    )
+  );
+};
+
+export const switchPasDfm = () => {
+  const editor = vscode.window.activeTextEditor;
+
+  if (!editor) {
+    return;
+  }
+
+  const textAtCursor = editor.document.getText(
+    editor.document.getWordRangeAtPosition(editor.selection.active)
+  );
+
+  const pathArray = editor.document.fileName.split(".");
+  const oldExt = (pathArray.pop() || "").toLowerCase();
+  const newPath =
+    oldExt === "pas"
+      ? [...pathArray, "dfm"].join(".")
+      : [...pathArray, "pas"].join(".");
+
+  vscode.workspace
+    .openTextDocument(newPath)
+    .then((doc) => vscode.window.showTextDocument(doc))
+    .then((editor) => {
+      if (oldExt === "pas") {
+        // only focus when switching pas -> dfm
+        focusWord(editor, textAtCursor);
+      }
+    });
+};
+
+export const generateDskFile = () => {
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    const pathArray = editor.document.fileName.split(".");
+    const ext = (pathArray.pop() || "").toLowerCase();
+    if (ext === "pas") {
+      vscode.workspace
+        .openTextDocument([...pathArray, "dfm"].join("."))
+        .then((doc) => vscode.window.showTextDocument(doc));
+    } else if (ext === "dfm") {
+      vscode.workspace
+        .openTextDocument([...pathArray, "pas"].join("."))
+        .then((doc) => vscode.window.showTextDocument(doc));
+    }
+  }
+};
